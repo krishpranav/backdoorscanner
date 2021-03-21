68d5447d8c93ee1e3f5fcd3e1e038d2a3f9fd0bc
krishpranav <krisna.pranav@gmail.com>
2021-03-21 17:13:39 +0530

    done changes

--- backdoorscan/backdoorscan.cpp
+++ backdoorscan/backdoorscan.cpp
@@ -15,3 +15,8 @@ std::ifstream inFile;
 
 std::string CheckFileTypes[] = {".lua", ".vmt", ".vtf", ".ttf"};
 
+constexpr unsigned int str2int(const char* str, int h = 0)
+{
+	return !str[h] ? 5381 : (str2int(str, h + 1) * 33) ^ str[h];
+}
+
